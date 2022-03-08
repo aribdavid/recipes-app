e0af3a4704f00c5e58f6bc19a6827b3080234554
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-03-08 15:53:58 -0300

    requisito 11

--- src/componets/Header/Header.jsx
+++ src/componets/Header/Header.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
 import profileIcon from '../../images/profileIcon.svg';
 import searchIcon from '../../images/searchIcon.svg';
 
 function Header({ title, searchBtn }) {
+  const history = useHistory();
+
+  const handleClick = () => {
+    history.push('/profile');
+  };
+
   return (
     <header>
-      <img data-testid="profile-top-btn" src={ profileIcon } alt="Profile Icon" />
+      <button type="button" onClick={ handleClick }>
+        <img
+          data-testid="profile-top-btn"
+          src={ profileIcon }
+          alt="Profile Icon"
+        />
+      </button>
       <h1 data-testid="page-title">{ title }</h1>
       { searchBtn && (
         <img data-testid="search-top-btn" src={ searchIcon } alt="Search Button" />
