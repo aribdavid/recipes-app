b3970a152c46c596b2d1adb373b1742a06137349
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-03-08 16:03:18 -0300

    requisito 12

--- src/componets/Header/Header.jsx
+++ src/componets/Header/Header.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { useHistory } from 'react-router-dom';
 import profileIcon from '../../images/profileIcon.svg';
 import searchIcon from '../../images/searchIcon.svg';
 
 function Header({ title, searchBtn }) {
+  const [showInput, setShowInput] = useState(false);
+  const [inputSearch, setinputSearch] = useState('');
   const history = useHistory();
 
   const handleClick = () => {
     history.push('/profile');
   };
 
+  const toggleInput = () => {
+    setShowInput(!showInput);
+  };
+
   return (
-    <header>
+    <header className="header-container">
       <button type="button" onClick={ handleClick }>
         <img
           data-testid="profile-top-btn"
@@ -22,7 +28,17 @@ function Header({ title, searchBtn }) {
       </button>
       <h1 data-testid="page-title">{ title }</h1>
       { searchBtn && (
-        <img data-testid="search-top-btn" src={ searchIcon } alt="Search Button" />
+        <button type="button" onClick={ toggleInput }>
+          <img data-testid="search-top-btn" src={ searchIcon } alt="Search Button" />
+        </button>
+      ) }
+      { showInput && (
+        <input
+          data-testid="search-input"
+          type="text"
+          value={ inputSearch }
+          onChange={ ({ target }) => setinputSearch(target.value) }
+        />
       ) }
     </header>
   );
