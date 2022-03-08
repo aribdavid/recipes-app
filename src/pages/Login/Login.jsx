3b2b2dd2f26975c9c95e0e5dfd76161517136652
aribdavid <aryehbdavid@gmail.com>
2022-03-08 14:29:49 -0300

    feat: requisitos 2,3 e 4

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,7 +2,19 @@ import React from 'react';
 
 function Login() {
   return (
-    <div>Login</div>
+    <main>
+      <form>
+        <label htmlFor="email">
+          Email
+          <input name="email" type="email" data-testid="email-input" />
+        </label>
+        <label htmlFor="password">
+          Senha
+          <input name="password" type="password" data-testid="password-input" />
+        </label>
+        <button type="submit" data-testid="login-submit-btn">Entrar</button>
+      </form>
+    </main>
   );
 }
 
