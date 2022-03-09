43184b0dc223123224237d954bf9f96b89c82f02
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-03-09 15:10:14 -0300

    requisito 23 e 24

--- src/componets/Footer/Footer.jsx
+++ src/componets/Footer/Footer.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import drinkIcon from '../../images/drinkIcon.svg';
 import exploreIcon from '../../images/exploreIcon.svg';
 import mealIcon from '../../images/mealIcon.svg';
 import './style.css';
 
 function Footer() {
+  const history = useHistory();
   return (
     <footer className="footer-container" data-testid="footer">
-      <button type="button">
+      <button
+        type="button"
+        onClick={ () => history.push('/drinks') }
+      >
         <img
           data-testid="drinks-bottom-btn"
           src={ drinkIcon }
@@ -15,7 +20,10 @@ function Footer() {
         />
       </button>
 
-      <button type="button">
+      <button
+        type="button"
+        onClick={ () => history.push('/explore') }
+      >
         <img
           data-testid="explore-bottom-btn"
           src={ exploreIcon }
@@ -23,7 +31,10 @@ function Footer() {
         />
       </button>
 
-      <button type="button">
+      <button
+        type="button"
+        onClick={ () => history.push('/foods') }
+      >
         <img
           data-testid="food-bottom-btn"
           src={ mealIcon }
