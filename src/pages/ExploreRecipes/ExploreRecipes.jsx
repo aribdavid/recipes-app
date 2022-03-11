6e014a37e885e301579ac0142271b100bfbfd470
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-03-11 16:20:28 -0300

    requisitos 70 ao 74

--- src/pages/ExploreRecipes/ExploreRecipes.jsx
+++ src/pages/ExploreRecipes/ExploreRecipes.jsx
@@ -1,17 +1,41 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 
 function ExploreRecipes() {
+  const [pathname, setPathName] = useState('');
   const history = useHistory();
+  const location = useLocation();
+
+  useEffect(() => {
+    setPathName(location.pathname.split('/')[2]);
+    console.log(pathname);
+  }, []);
 
   return (
     <main className="explore-recipes-container">
       <button
         type="button"
         data-testid="explore-by-ingredient"
-        onClick={ () => history.push} }
+        onClick={ () => history.push(`/explore/${pathname}/ingredients`) }
       >
         By Ingredient
+      </button>
+
+      { pathname === 'foods' && (
+        <button
+          type="button"
+          data-testid="explore-by-nationality"
+          onClick={ () => history.push(`/explore/${pathname}/nationalities`) }
+        >
+          By Nationality
+        </button>
+      ) }
+
+      <button
+        type="button"
+        data-testid="explore-surprise"
+      >
+        Surprise me!
       </button>
     </main>
   );
