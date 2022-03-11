8d66c7f224ddadac097da97e3ccf4b45a92b5148
MuriloAmaral <muralha2000@gmail.com>
2022-03-11 12:46:53 -0300

    requisito 60 ao 66

---
+++ src/pages/FavRecipes/FavRecipes.jsx
@@ -0,0 +1,51 @@
+/* eslint-disable react/jsx-no-bind */
+import React from 'react';
+import FavoriteRecipes from '../../componets/FavoriteRecipes/FavoriteRecipes';
+
+function FavoritesRecipes() {
+  const [favoriteRecipes, setFavoriteRecipes] = useState([]);
+  const [filteredFavoriteRecipes, setFilteredFavoriteRecipes] = useState([]);
+
+  function handleClick(event) {
+    const { name } = event.target;
+    const getFavoriteRecipes = favoriteRecipes.filter((recipe) => (
+      recipe.type.includes(name)
+    ));
+    setFilteredFavoriteRecipes(getFavoriteRecipes);
+  }
+
+  function unFavorite(event) {
+    const { name } = event.target;
+    const filteredRecipes = favoriteRecipes.filter((recipe) => (
+      recipe.name !== name
+    ));
+    localStorage.setItem('favoriteRecipes', JSON.stringify(filteredRecipes));
+    const favoriteRecipesLocalStorage = JSON.parse(localStorage
+      .getItem('favoriteRecipes'));
+    setFavoriteRecipes(favoriteRecipesLocalStorage);
+    setFilteredFavoriteRecipes(favoriteRecipesLocalStorage);
+  }
+
+  useEffect(() => {
+    if (!localStorage.getItem('favoriteRecipes')) {
+      localStorage.setItem('favoriteRecipes', JSON.stringify([]));
+    } else {
+      const favoriteRecipesLocalStorage = JSON.parse(localStorage
+        .getItem('favoriteRecipes'));
+      setFavoriteRecipes(favoriteRecipesLocalStorage);
+      setFilteredFavoriteRecipes(favoriteRecipesLocalStorage);
+    }
+  }, []);
+
+  return (
+    <div>
+      <FavoriteRecipes
+        handleClick={ handleClick }
+        filteredFavoriteRecipes={ filteredFavoriteRecipes }
+        unFavorite={ unFavorite }
+      />
+    </div>
+  );
+}
+
+export default FavoritesRecipes;
--- src/pages/FavoritesRecipes/FavoritesRecipes.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function FavoritesRecipes() {
-  return (
-    <div>FavoriteRecipes</div>
-  );
-}
-
-export default FavoritesRecipes;
