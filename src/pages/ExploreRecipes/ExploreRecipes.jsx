8185af6510750e70c0e15406176577a0e8bc1d5b
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-03-11 14:52:43 -0300

    começo do requisito

--- src/pages/ExploreRecipes/ExploreRecipes.jsx
+++ src/pages/ExploreRecipes/ExploreRecipes.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 function ExploreRecipes() {
+  const history = useHistory();
+
   return (
-    <div>ExploreRecipes</div>
+    <main className="explore-recipes-container">
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+        onClick={ () => history.push} }
+      >
+        By Ingredient
+      </button>
+    </main>
   );
 }
 
