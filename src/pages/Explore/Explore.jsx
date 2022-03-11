5630250d8d43a007fda23dbec3c871a6b19b32dd
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-03-11 14:46:29 -0300

    tela de explorar done

--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 function Explore() {
+  const history = useHistory();
+
   return (
-    <div>Explore</div>
+    <main className="explore-container">
+      <button
+        type="button"
+        data-testid="explore-foods"
+        onClick={ () => history.push('/explore/foods') }
+      >
+        Explore Foods
+      </button>
+
+      <button
+        type="button"
+        data-testid="explore-drinks"
+        onClick={ () => history.push('/explore/drinks') }
+      >
+        Explore Drinks
+      </button>
+    </main>
   );
 }
 
