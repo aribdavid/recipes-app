6f092fc216910bfea218adfe116b0a99cea4d10d
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-03-14 13:38:04 -0300

    teste da tela de detalhes (done), 72.22% da cobertura total

--- src/tests/09-detailsRecipe.test.js
+++ src/tests/09-detailsRecipe.test.js
@@ -7,6 +7,7 @@ import 'jest-localstorage-mock';
 
 const PATHNAME_FOODS = '/foods/52977';
 const DATA_TEST_START_BTN = 'start-recipe-btn';
+const FAVORITE_BTN = 'favorite-btn';
 
 describe('Testa a tela de detalhes de bebida e comida', () => {
   it('Todos os componentes estão na tela de comidas', async () => {
@@ -15,7 +16,7 @@ describe('Testa a tela de detalhes de bebida e comida', () => {
     const recipeImage = await screen.findByTestId('recipe-photo');
     const recipeTitle = await screen.findByTestId('recipe-title');
     const shareBtn = await screen.findByTestId('share-btn');
-    const favoriteBtn = await screen.findByTestId('favorite-btn');
+    const favoriteBtn = await screen.findByTestId(FAVORITE_BTN);
     const ingredients = await screen.findAllByTestId(/ingredient-name-and-measure/i);
     const instructions = await screen.findByTestId('instructions');
     const video = await screen.findByTestId('video');
@@ -46,7 +47,7 @@ describe('Testa a tela de detalhes de bebida e comida', () => {
   it('Verifica o botão de favoritar ', async () => {
     const { history } = renderWithRouterAndContext(<App />);
     history.push(PATHNAME_FOODS);
-    const favoriteBtn = await screen.findByTestId('favorite-btn');
+    const favoriteBtn = await screen.findByTestId(FAVORITE_BTN);
     userEvent.click(favoriteBtn);
   });
 
@@ -72,6 +73,19 @@ describe('Testa a tela de detalhes de bebida e comida', () => {
       history.push('/foods/52785');
       const startRecipe = await screen.findByTestId(DATA_TEST_START_BTN);
       expect(startRecipe.innerHTML).toBe('Continue Recipe');
+    },
+  );
+
+  it(
+    'Verifica se é possivel favoritar uma receita',
+    async () => {
+      const { history } = renderWithRouterAndContext(<App />);
+      history.push('/foods/53026');
+      const favoriteBtn = await screen.findByTestId(FAVORITE_BTN);
+      localStorage.clear();
+      userEvent.click(favoriteBtn);
+      const favRecipeInLocalStorage = JSON.parse(localStorage.getItem('favoriteRecipes'));
+      expect(favRecipeInLocalStorage[0].id).toBe('53026');
     },
   );
 });
